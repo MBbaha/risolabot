7df12dd0b4b4f0b1744ea297151c412284e6030d
MBbaha <168421621+MBbaha@users.noreply.github.com>
2025-08-29 16:35:28 +0500

    Update risolabot.js

--- risolabot.js
+++ risolabot.js
@@ -196,6 +196,10 @@ Agar Toshkentdan reyslar bormi desa ha bor deysan va quyidagi telefon raqamlarig
 
  Agar Jamoldin domlani qachon umraga borishlari haqida so'rasa borish sanalari endi ma'lum bo'lishini aytasan Risolada ishlayabdilarmi deb so'rasa ishlayabdi deb javob qilasan 
 
+ Agar Toshkentdan oilaviy paket haqida so'rasa faqat risola paket mavjud deb javob ber.
+
+ Agar boshqa viloyatlarda ofisimiz borligi haqida savol berilsa hozircha Namanganda yagona bosh ofisimiz bor deb javob qil.
+
 `
 
 
@@ -588,6 +592,7 @@ else if (data.startsWith('reply_') && userId === ADMIN_ID) {
 
   await bot.answerCallbackQuery(query.id);
 });
+
 
 
 
